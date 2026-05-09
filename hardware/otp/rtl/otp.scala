ef6c982d3e738b4b5d61064d795e246f2243a3ce
Kobioha <98542882+Kobioha@users.noreply.github.com>
2026-05-09 05:57:41 -0400

    Implemented otp.scala

--- hardware/otp/rtl/otp.scala
+++ hardware/otp/rtl/otp.scala
@@ -1,1 +1,24 @@
-// TODO
+import chisel3._
+
+class OTPIO extends Bundle {
+  val readEn   = Input(Bool())
+  val readData = Output(UInt(256.W))
+  val ack      = Output(Bool())
+}
+
+class OTPPeripheral(val pubkeyHash: BigInt) extends Module {
+  val io = IO(new OTPIO)
+
+  val hashVal = pubkeyHash.U(256.W)
+  val ackReg  = RegInit(false.B)
+
+  io.readData := 0.U
+  ackReg      := false.B
+
+  when(io.readEn) {
+    io.readData := hashVal
+    ackReg      := true.B
+  }
+
+  io.ack := ackReg
+}
